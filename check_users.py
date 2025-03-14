05eba71a82e9d7f9204b80396269bf6fb57cdc57
Mohammed Fahad Nawaz Khan <84392910+rosepanther14@users.noreply.github.com>
2025-03-15 04:49:42 +0530

    Update check_users.py

--- check_users.py
+++ check_users.py
@@ -5,11 +5,11 @@ import numpy as np
 conn = sqlite3.connect("attendance.db")
 c = conn.cursor()
 
-# Fetch all users
+
 c.execute("SELECT name, encoding FROM users")
 users = c.fetchall()
 
-# Display stored users
+
 for user in users:
     name = user[0]
     encoding = np.frombuffer(user[1], dtype=np.float64)  # Convert bytes to array
